db13ff8cbd15691b209694eff0006cd36ed1f25f
Gustavo Nunes <gustavonunesedl@gmail.com>
2017-03-31 03:06:00 -0300

    fix code style

--- src/status_im/transactions/styles/list_item.cljs
+++ src/status_im/transactions/styles/list_item.cljs
@@ -3,31 +3,31 @@
   (:require [status-im.components.styles :as st]))
 
 (def item
-  {:padding-vertical 20
+  {:padding-vertical   20
    :padding-horizontal 16
-   :flex 1
-   :flex-direction :row
-   :align-items :center})
+   :flex               1
+   :flex-direction     :row
+   :align-items        :center})
 
 (def item-photo
-  {:width 86
-   :height 48
-   :border-radius 100
+  {:width            86
+   :height           48
+   :border-radius    100
    :background-color st/color-dark-blue-3
-   :flex-direction :row
-   :align-items :center})
+   :flex-direction   :row
+   :align-items      :center})
 
 (def item-info
   {:margin-left 16
-   :flex 1})
+   :flex        1})
 
 (def item-info-recipient
-  {:color st/color-light-blue
-   :font-size 15
+  {:color       st/color-light-blue
+   :font-size   15
    :flex-shrink 1})
 
 (def item-info-amount
-  {:color st/color-white
+  {:color     st/color-white
    :font-size 19})
 
 (def item-deny-btn
@@ -44,5 +44,5 @@
 
 (def item-photo-icon
   {:margin-left 4
-   :width 24
-   :height 24})
+   :width       24
+   :height      24})
